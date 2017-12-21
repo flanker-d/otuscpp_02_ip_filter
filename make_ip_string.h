4e35d08af9dec090877c703969273a7cd4d8547c
Artyom Sorokoumov <artyom.sorokoumov@gmail.com>
2017-12-21 17:29:50 +0300

    some fixes

--- make_ip_string.h
+++ make_ip_string.h
@@ -11,28 +11,20 @@ struct gen_seq : gen_seq<N-1, N-1, Is...>{};
 template<std::size_t... Is>
 struct gen_seq<0, Is...> : seq<Is...>{};
 
-template<class Ch, class Tr, class Tuple, std::size_t... Is>
+template<typename Ch, typename Tr, typename Tuple, std::size_t... Is>
 void print_tuple(std::basic_ostream<Ch,Tr>& os, Tuple const& t, seq<Is...>){
   using swallow = int[];
   (void)swallow{0, (void(os << (Is == 0? "" : ".") << std::get<Is>(t)), 0)...};
 }
 
-template<class Ch, class Tr, class... Args>
-auto operator<<(std::basic_ostream<Ch, Tr>& os, std::tuple<Args...> const& t) -> std::basic_ostream<Ch, Tr>&
-{
-  os << "(";
-  print_tuple(os, t, gen_seq<sizeof...(Args)>());
-  return os << ")";
-}
-
-template<class Ch, class Tr, class... Args>
+template<typename Ch, typename Tr, typename... Args>
 auto operator<<(std::basic_stringstream<Ch, Tr>& ss, std::tuple<Args...> const& t) -> std::basic_stringstream<Ch, Tr>&
 {
   print_tuple(ss, t, gen_seq<sizeof...(Args)>());
   return ss;
 }
 
-template<class... Args>
+template<typename... Args>
 std::string make_ip_string(std::tuple<Args...> const& t)
 {
   std::stringstream ss;
